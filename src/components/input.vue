4e4b8e6eb0b48079d29a33e936193191807a0fc7
zhangxl <zhangxl37@lenovo.com>
2019-11-01 22:43:20 +0800

    input组件

--- src/components/input.vue
+++ src/components/input.vue
@@ -1,6 +1,14 @@
 <template>
+    <!-- 
+        input 组件：
+            // 1. 是有label   
+            // 2. 可以设置labelwidth 
+            3. 可以 用 v-model
+            4. 可以触发change，input 等事件
+            5.应该有setVal 和 getVal这俩个方法
+     -->
     <div>
-        <span class="asLabel" v-show="label" :style="{width:zlabelwidth}">{{label}}</span>
+        <span class="asLabel" v-show="label" :style="{width:zlabelWidth}">{{label}}</span>
         <input ref="zinput" type="text" :value="value" @input="throwValue($event)" @change="returnChangeval"/>
     </div>
 </template>
@@ -8,17 +16,35 @@
 <script>
 import $emitVue from './$emit.vue';
 export default {
-    props:["label","labelwidth","value"],
+    props:["label","labelWidth","value"],
     data(){
         return{
 
         }
     },
     methods:{
-       
+        setVal(val){
+            this.value = val;
+        },
+        getVal(){
+            return this.$refs.zinput.value;
+        },
+        returnChangeval(event){
+            this.$emit("change",event.target.value)
+        },
+        throwValue(event){
+            this.$emit("input",event.target.value)
+        }
+
     },
     computed:{
-       
+        zlabelWidth(){
+            if(this.labelWidth && this.labelWidth > 0 ){
+                return this.labelWidth + "px";
+            }else{
+                return "120px";
+            }
+        }
     }
 }
 </script>
@@ -29,4 +55,15 @@ export default {
     text-align: right;
     font-size: 16px;
 }
+
+input{
+    width: 200px;
+    height: 26px;
+    line-height: 26px;
+    border-radius: 3px;
+    border: none;
+    border: 1px solid #ccc;
+    padding: 0 10px;
+}
+
 </style>
--- src/components/usePage.vue
+++ src/components/usePage.vue
@@ -1,7 +1,13 @@
 <template>
-    <div>
+    <div style="height:100%">
         <p class="pageName">使用组件的集合</p>
-        
+        <div class="templateBox">
+            <div class="template-item">
+                <h3 class="templateName">input组件的使用:</h3>
+                <zInput label="剧名:" labelWidth="120" v-model="defaultName"></zInput>
+                <p>{{"使用v-model后的值:   "+defaultName}}</p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,7 +17,7 @@ export default {
     components:{zInput},
     data(){
         return {
-            
+            defaultName:'我们的幸福生活'
         }
     },
     methods:{
@@ -24,5 +30,15 @@ export default {
 </script>
 
 <style scoped>
+.templateBox{
+    background-color: #fff;
+    margin-bottom: 50px;
+}
 
+.template-item{
+    min-height: 200px;
+    margin-bottom: 10px;
+    padding-bottom: 10px;
+    border-bottom: 1px solid pink;
+}
 </style>
