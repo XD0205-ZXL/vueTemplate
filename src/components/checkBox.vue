d1d6a30fbb8e4796b56dec584124087f796b71bf
zhangxl <zhangxl37@lenovo.com>
2019-11-17 10:40:00 +0800

    多选框

---
+++ src/components/checkBox.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <!-- 
+            1. 有 labelWidth 
+            2. 有 v-model 是用value来实现
+            3. 有 DataSource
+            4. 有 多选功能
+            5. 有 clear功能
+         -->
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
